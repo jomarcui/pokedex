858777aee2d3a7f79e081b35031e07e968d9dc03
jomarcui <cuijomar@gmail.com>
2024-02-22 20:47:28 +0800

    style: update page layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,18 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
+    <main className="flex min-h-screen flex-col p-24">
+      <div className="w-full">
         <Image
           alt="Pokémon"
+          className="mx-auto"
           height={300}
           priority
           src="/pokemon.svg"
           width={300}
         />
       </div>
+      <div className="grow self-start w-full">Test</div>
     </main>
   );
 }
